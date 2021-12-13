74fb65f603ec1758ed3ad7b67c4a32e63b6f2842
Hrnrjks <empty@email>
2021-12-13 21:12:31 +0200

    Replaced mobile navbar code, different mobile navbar

--- scripts/js.js
+++ scripts/js.js
@@ -1,3 +1,20 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open')
 }
+
+function toggleBurger() {
+    var x = document.getElementsByClassName("mobile-menu-list")
+    if (x.style.display === "block") {
+        x.style.display = "none"
+    } else {
+        x.style.display = "block"
+    }
+}
+
+$(".checkbox-burger").on("change", function () {
+    if ($(this).prop("checked")) {
+        $("body").css("overflow", "hidden")
+    } else {
+        $("body").css("overflow", "visible")
+    }
+})
