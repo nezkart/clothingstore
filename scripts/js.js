04ddca42fc605c6c89ceae357d06701fe123e76b
Hrnrjks <empty@email>
2021-12-14 00:21:51 +0200

    Load js on document ready jquery

--- scripts/js.js
+++ scripts/js.js
@@ -1,24 +1,26 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open')
-}
-
-function toggleBurger() {
-    var x = document.getElementsByClassName("mobile-menu-list")
-    if (x.style.display === "block") {
-        x.style.display = "none"
-    } else {
-        x.style.display = "block"
+$(document).ready(function() {
+    function toggleMobileMenu(menu) {
+        menu.classList.toggle('open')
     }
-}
-
-$(".checkbox-burger").on("change", function () {
-    if ($(this).prop("checked")) {
-        $("body").css("overflow", "hidden")
-        $(".mobile-cart-a").css("z-index", 0)
-        $("#mobile-logo").css("z-index", 0)
-    } else {
-        $("body").css("overflow", "visible")
-        $(".mobile-cart-a").css("z-index", 1)
-        $("#mobile-logo").css("z-index", 1)
+    
+    function toggleBurger() {
+        var x = document.getElementsByClassName("mobile-menu-list")
+        if (x.style.display === "block") {
+            x.style.display = "none"
+        } else {
+            x.style.display = "block"
+        }
     }
+    
+    $(".checkbox-burger").on("change", function () {
+        if ($(this).prop("checked")) {
+            $("body").css("overflow", "hidden")
+            $(".mobile-cart-a").css("z-index", 0)
+            $("#mobile-logo").css("z-index", 0)
+        } else {
+            $("body").css("overflow", "visible")
+            $(".mobile-cart-a").css("z-index", 1)
+            $("#mobile-logo").css("z-index", 1)
+        }
+    })
 })
