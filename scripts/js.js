1d1ed3fd03cb9d10616b141097d6ae2f44d17384
Hrnrjks <empty@email>
2021-12-14 00:10:27 +0200

    Improved on mobile navbar, added cart icon...

--- scripts/js.js
+++ scripts/js.js
@@ -14,7 +14,11 @@ function toggleBurger() {
 $(".checkbox-burger").on("change", function () {
     if ($(this).prop("checked")) {
         $("body").css("overflow", "hidden")
+        $(".mobile-cart-a").css("z-index", 0)
+        $("#mobile-logo").css("z-index", 0)
     } else {
         $("body").css("overflow", "visible")
+        $(".mobile-cart-a").css("z-index", 1)
+        $("#mobile-logo").css("z-index", 1)
     }
 })
